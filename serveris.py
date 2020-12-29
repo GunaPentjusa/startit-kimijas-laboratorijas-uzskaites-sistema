463f1892d62295f916c318d88412b62141809810
Guna Pentjusa <zemiiteg@gmail.com>
2020-12-29 11:56:04 +0000

    izmaiņas 2

--- serveris.py
+++ serveris.py
@@ -8,10 +8,34 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/publiski')
+def publiski():
+    return render_template("pub_data.html")
+
+
 @app.route('/pieslegties')
 def pieslegties():
     return render_template("login.html")
 
+
+@app.route('/uzskaite')
+def uzskaite():
+    return render_template("vielu_aprikojuma_uzskaite.html")
+
+
+@app.route('/pievienot')
+def pievienot():
+    return render_template("pievienot_vielu_aprikojumu.html")
+
+
+@app.route('/lietotajs')
+def lietotajs():
+    return render_template("user_menu.html")
 
 
 @app.route('/api/v1/vielas')
